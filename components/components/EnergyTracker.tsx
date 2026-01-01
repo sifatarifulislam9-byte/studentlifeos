964e2e83acbdb682be40f47f532c3399173bb100
sifatarifulislam9-byte <sifatarifulislam9@gmail.com>
2026-01-01 21:50:04 +0600

    Delete components directory

--- components/components/EnergyTracker.tsx
+++
@@ -1,9 +0,0 @@
-export default function EnergyTracker() {
-  const energy = 78;
-  return (
-    <div className="mt-6 p-4 bg-white rounded-xl shadow">
-      <h3>Daily Energy</h3>
-      <p className="text-2xl font-bold">{energy}%</p>
-    </div>
-  );
-}
